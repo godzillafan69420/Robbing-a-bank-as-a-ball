2d4fed5519b70fe1e00dca45ce7a3723d3c1224e
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-02-13 10:21:52 +1300

    ow

--- robbing-a-bank-as-a-ball/scripts/gun_pivot.gd
+++ robbing-a-bank-as-a-ball/scripts/gun_pivot.gd
@@ -4,26 +4,41 @@ const bulletPrefab = preload("res://scenes/bullet.tscn")
 var player
 var canShoot = true
 var degrees
+var burstShot: int = 2
+var reloading: bool = false
 
 func _ready() -> void:
 	player = get_parent()
 func _process(delta: float) -> void:
+	
+	print(burstShot)
+	print(canShoot)
 	degrees = atan2(get_global_mouse_position().y - $"..".position.y, get_global_mouse_position().x - $"..".position.x)
 	$".".rotation =  degrees
 	if (abs(rad_to_deg(degrees)) < 270) && (abs(rad_to_deg(degrees)) > 90):
 		$Sprite2D.flip_v = true
 	else:
 		$Sprite2D.flip_v = false
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && canShoot:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canShoot and burstShot > 0:
 		for i in range(15):
 			var bullet = bulletPrefab.instantiate()
 			bullet.position = player.position
 			player.get_parent().add_child(bullet)
 			bullet.rotation = deg_to_rad((i*2) +rad_to_deg(degrees)-10)
-		canShoot = false
 		$"../shootTimer".start()
+		burstShot -= 1
+		canShoot = false
+	if burstShot < 1 and !reloading:
+		reloading = true
+		$"../reloadTime".start()
 		
 
 
 func _on_shoot_timer_timeout() -> void:
 	canShoot = true
+
+
+func _on_reload_time_timeout() -> void:
+	burstShot = 2
+	canShoot = true
+	reloading = false
--- robbing-a-bank-as-a-ball/scripts/player.gd
+++ robbing-a-bank-as-a-ball/scripts/player.gd
@@ -1,28 +1,37 @@
 extends CharacterBody2D
 
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const MAXSPEED = 150
+const SPEED = 25.0
+const shotgunStrength = 670
 enum States {ragdoll, walking, dead, idle}
 var Currentstate = States.idle
 var mouseposition: Vector2
 var mouseDirection: Vector2
 var canJump: bool = true
+var burstCount: int = 2
+var momentum: float
 func _process(delta: float) -> void:
 	mouseposition = get_global_mouse_position()
 	mouseDirection = mouseposition - position
+	
 	
 		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	
+	print("burst: " + str(burstCount))
+
+	var direction := Input.get_axis("left", "right")
+	momentum = SPEED * 0.9 * direction
+	if momentum > MAXSPEED:
+		momentum = MAXSPEED
+	if momentum < -MAXSPEED:
+		momentum = -MAXSPEED
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	
 	if direction == 1:
 		$direction.scale = Vector2(1,1)
 	elif direction == -1:
@@ -32,22 +41,37 @@ func _physics_process(delta: float) -> void:
 	else:
 		Currentstate = States.idle
 	if Currentstate == States.walking:
-		velocity.x = direction * SPEED
+		
 		$direction/AnimatedSprite2D.stop()
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canJump:
-			velocity = mouseDirection.normalized() * 500 * -1
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canJump and burstCount > 0:
+			velocity = mouseDirection.normalized() * shotgunStrength * -1
+			
 			canJump = false
+			burstCount -= 1
+		
+		velocity.x += momentum
+		if velocity.x > 400:
+			velocity.x = 400
+		if velocity.x < -400:
+			velocity.x = -400
 	else:
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canJump:
-			velocity = mouseDirection.normalized() * 500 * -1
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canJump and burstCount > 0:
+			velocity = mouseDirection.normalized() * shotgunStrength * -1
 			canJump = false
+			burstCount -= 1
 		elif Currentstate != States.walking and is_on_floor():
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			$direction/AnimatedSprite2D.play("idle")
+	
 	
 		
 	move_and_slide()
 
 
 func _on_shoot_timer_timeout() -> void:
+	canJump = true
+
+
+func _on_reload_time_timeout() -> void:
+	burstCount = 2
 	canJump = true
