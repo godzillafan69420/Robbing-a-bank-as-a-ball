6a3da72ec2becac7bd2532ef0bc44a7e830d9014
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-03-05 20:34:21 +1300

    nah

--- robbing-a-bank-as-a-ball/scenes/win_screen.gd
+++ robbing-a-bank-as-a-ball/scenes/win_screen.gd
@@ -11,20 +11,21 @@ func _ready() -> void:
 	$bestTimre.text = "Best time: " + str(round(Global.topTime))
 	if Global.STAGE1time < 30:
 		$message.text = "what tech is that"
-	elif Global.STAGE1time < 32:
-		$message.text = "huh how"
-	elif Global.STAGE1time == 32:
-		$message.text = "my Score"
+	elif Global.STAGE1time < 40:
+		$message.text = "he might be the goat"
 	elif Global.STAGE1time < 50:
-		$message.text = "ok nice"
+		$message.text = "you tried your best.
+		You're trash!!!"
 	elif Global.STAGE1time < 60:
-		$message.text = "took you a minute"
+		$message.text = "almost hit a minute. almost"
+	
 	elif Global.STAGE1time < 70:
-		$message.text = "you're buns"
+		$message.text = "have a participation reward"
 	elif Global.STAGE1time == 67:
-		$message.text = "67676767676767676767676767"
+		$message.text = "mango, mustard, burger 67 "
 	elif Global.STAGE1time >= 70:
-		$message.text = "you make caseoh look fast"
+		$message.text = "in the end the bank is still
+		dumb enough to not move the money"
 
 
 func _on_button_button_down() -> void:
